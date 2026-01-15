7d7863adf67956237281429ffff190c238c3bac4
AchinthaThennakoon <achinthathennakoon97@gmail.com>
2026-01-15 10:11:40 +0530

    initial commit

---
+++ cmd/migrate/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration direction: 'up' or 'down'")
+	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	instance, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	if err != nil {
+		log.Fatalf("Failed to create database instance: %v", err)
+	}
+
+	fSrc, err := (&file.File{}).Open("cmd/migrate/migrations")
+	if err != nil {
+		log.Fatalf("Failed to open migration files: %v", err)
+	}
+
+	m, err := migrate.NewWithInstance(
+		"file",
+		fSrc,
+		"sqlite3",
+		instance,
+	)
+	if err != nil {
+		log.Fatalf("Failed to create migrate instance: %v", err)
+	}
+
+	switch direction {
+	case "up":
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatalf("Migration up failed: %v", err)
+		}
+	case "down":
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatalf("Migration down failed: %v", err)
+		}
+	default:
+		log.Fatal("Invalid direction. Use 'up' or 'down'")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module rest-api-gin-go
+
+go 1.25.5
+
+require github.com/golang-migrate/migrate/v4 v4.19.1
