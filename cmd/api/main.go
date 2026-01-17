ce545ee950678f49cee7e6e9da6c4f496c469eec
AchinthaThennakoon <achinthathennakoon97@gmail.com>
2026-01-17 20:27:54 +0530

    set env files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,15 +3,36 @@ package main
 import (
 	"database/sql"
 	"log"
+	"rest-api-gin-go/internal/database"
+	"rest-api-gin-go/internal/env"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type application struct {
+	port      int
+	jwtSecret string
+	models database.Models
+}
 
 func main() {
 	// create database connection
-	db, err := sql.Open("sqlite3","./data.db")
+	db, err := sql.Open("sqlite3", "./data.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
+	models := database.NewModels(db)
 
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "your_jwt_secret"),
+		models:    models,
+	}
+
+	if err := app.server(); err != nil {
+		log.Fatal(err)
+	}
 }
