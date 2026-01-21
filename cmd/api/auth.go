286792518f061adc200c5b5343d03c40d0bf0cb2
AchinthaThennakoon <achinthathennakoon97@gmail.com>
2026-01-21 20:51:33 +0530

    Created register user

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"hash"
 	"net/http"
 	"rest-api-gin-go/internal/database"
 
@@ -9,22 +8,39 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type registerRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required,min=2"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
 func (app *application) registerUser(c *gin.Context) {
 	// Implementation for user registration will go here
-	var user database.User
-	if err := c.ShouldBindJSON(&user); err != nil {
+	var register registerRequest
+	if err := c.ShouldBindJSON(&register); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	// hash the password
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(register.Password), bcrypt.DefaultCost)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
 		return
 	}
-	user.Password = string(hashedPassword)
+	register.Password = string(hashedPassword)
 
-	
+	user := database.User{
+		Email:    register.Email,
+		Name:     register.Name,
+		Password: register.Password,
+	}
 
+	err = app.models.Users.Insert(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
 }
