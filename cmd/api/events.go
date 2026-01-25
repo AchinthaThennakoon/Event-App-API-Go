7a56ded9b901555e95da0e66064381ecf870b89f
AchinthaThennakoon <achinthathennakoon97@gmail.com>
2026-01-25 17:31:50 +0530

    add crud for events table

--- cmd/api/events.go
+++ cmd/api/events.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"rest-api-gin-go/internal/database"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,4 +22,72 @@ func (app *application) createEvent(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, event)
+}
+
+func (app *application) getAllEvents(c *gin.Context) {
+	events, err := app.models.Events.GetAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve events"})
+		return
+	}
+
+	c.JSON(http.StatusOK, events)
+}
+
+func (app *application) getEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	event, err := app.models.Events.GetByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+
+	c.JSON(http.StatusOK, event)
+}
+
+func (app *application) updateEvent(c *gin.Context) {
+	// var event database.Event
+	// if err := c.ShouldBindJSON(&event); err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 	return
+	// }
+
+
+
+	// if err := app.models.Events.Update(&event); err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update event"})
+	// 	return
+	// }
+	// c.JSON(http.StatusOK, event)
+}
+
+func (app *application) deleteEvent(c *gin.Context) {
+	//delete event logic here
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	existingEvent, err := app.models.Events.GetByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+	if existingEvent == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+		return
+	}
+
+	if err := app.models.Events.Delete(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete event"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully"})
+
 }
--- internal/database/events.go
+++ internal/database/events.go
@@ -27,3 +27,62 @@ func (m EventModel) Insert(event *Event) any {
 
 	return m.DB.QueryRowContext(ctx, query, event.OwnerID, event.Name, event.Description, event.Date, event.Location).Scan(&event.ID)
 }
+
+func (m EventModel) GetAll() (any, any) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "SELECT id, owner_id, name, description, date, location FROM events"
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	events := []*Event{}
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.OwnerID, &event.Name, &event.Description, &event.Date, &event.Location)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, &event)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
+
+func (m EventModel) GetByID(id int) (any, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "SELECT id, owner_id, name, description, date, location FROM events WHERE id = $1"
+
+	var event Event
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(&event.ID, &event.OwnerID, &event.Name, &event.Description, &event.Date, &event.Location)
+	if err != nil {
+		return nil, err
+	}
+	return &event, nil
+}
+
+func (m EventModel) Delete(id int) any {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "DELETE FROM events WHERE id = $1"
+
+	_, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
